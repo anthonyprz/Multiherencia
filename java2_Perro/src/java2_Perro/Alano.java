06c7a8106a237cee75748fa699f396eb195e1ed4
Sonia Ortiz de Arri <soniaodea@gmail.com>
2015-01-27 09:20:34 +0100

    listo

---
+++ java2_Perro/src/java2_Perro/Alano.java
@@ -0,0 +1,23 @@
+package java2_Perro;
+
+public class Alano extends Perro implements Lebrel, Dogo {
+
+	@Override
+	void ladrar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void proteger() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void correr() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ java2_Perro/src/java2_Perro/Perro.java
@@ -0,0 +1,8 @@
+package java2_Perro;
+
+public abstract class Perro {
+	abstract void ladrar();
+	void lamer(){
+		System.out.println("lame");
+	}
+}
